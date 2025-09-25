32e35d77a2dbcd5019026e520f05d1aa8461d601
John Sell <jsell@redhat.com>
2025-09-25 15:47:04 -0400

    chore: Fix linting warning.

--- backend/kg/core/__init__.py
+++ backend/kg/core/__init__.py
@@ -13,16 +13,13 @@ from .schema_loader import FileSchemaLoader, SchemaLoader
 
 # Export all schema-related components
 __all__ = [
-    # Schema data structures
-    "FieldDefinition",
-    "RelationshipDefinition",
     "EntitySchema",
-    "SchemaLoadResult",
-    # Schema exceptions
-    "SchemaValidationError",
-    "SchemaLoadError",
-    "SchemaInheritanceError",
-    # Schema loaders
-    "SchemaLoader",
+    "FieldDefinition",
     "FileSchemaLoader",
+    "RelationshipDefinition",
+    "SchemaInheritanceError",
+    "SchemaLoadError",
+    "SchemaLoadResult",
+    "SchemaLoader",
+    "SchemaValidationError",
 ]
